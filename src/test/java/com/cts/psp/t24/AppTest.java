71725e3dc1393f4e1132d14142f7bbb16c038e86
admin <admin@test.com>
2017-02-21 14:12:01 +0530

    renmaing

--- src/test/java/com/cts/psp/t24/AppTest.java
+++ src/test/java/com/cts/psp/t24/AppTest.java
@@ -14,7 +14,7 @@ import com.work.fb.FbRoutesApplication;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {FbRoutesApplication.class})
-public class T24AdapterBaseApplicationTests {
+public class AppTest {
 	
 	@Inject
 	AppService service;
